1d9ed3b6ba5a267f0fe0ab784d76e3acd7ecb639
Arthur Schiwon <blizzz@arthur-schiwon.de>
2025-08-10 01:04:12 +0200

    fix: use web component for workflow config
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- src/flow.js
+++ src/flow.js
@@ -1,8 +1,21 @@
+import wrap from '@vue/web-component-wrapper'
+import Vue from 'vue'
+
 import WorkflowKitinerary from './WorkflowKitinerary.vue'
+
+const FlowKItineraryComponent = wrap(Vue, WorkflowKitinerary)
+const customElementId = 'oca-workflow_kitinerary-operation-import'
+
+window.customElements.define(customElementId, FlowKItineraryComponent)
+
+// In Vue 2, wrap doesn't support disabling shadow :(
+// Disable with a hack
+Object.defineProperty(FlowKItineraryComponent.prototype, 'attachShadow', { value() { return this } })
+Object.defineProperty(FlowKItineraryComponent.prototype, 'shadowRoot', { get() { return this } })
 
 OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\WorkflowKitinerary\\Operation',
 	operation: '',
-	options: WorkflowKitinerary,
+	element: customElementId,
 	color: '#4d4d4d',
 })
