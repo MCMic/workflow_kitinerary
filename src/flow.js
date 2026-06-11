313b05b0e3aa4711c90382238ec74e0c7d5e80e6
Côme Chilliet <come@chilliet.eu>
2026-06-11 18:08:14 +0200

    feat: Upgrade to vue3
    Signed-off-by: Côme Chilliet <come@chilliet.eu>

--- src/flow.js
+++ src/flow.js
@@ -1,17 +1,11 @@
-import wrap from '@vue/web-component-wrapper'
-import Vue from 'vue'
+import { defineCustomElement } from 'vue'
 
 import WorkflowKitinerary from './WorkflowKitinerary.vue'
 
-const FlowKItineraryComponent = wrap(Vue, WorkflowKitinerary)
+const FlowKItineraryComponent = defineCustomElement(WorkflowKitinerary, { shadowRoot: false })
 const customElementId = 'oca-workflow_kitinerary-operation-import'
 
 window.customElements.define(customElementId, FlowKItineraryComponent)
-
-// In Vue 2, wrap doesn't support disabling shadow :(
-// Disable with a hack
-Object.defineProperty(FlowKItineraryComponent.prototype, 'attachShadow', { value() { return this } })
-Object.defineProperty(FlowKItineraryComponent.prototype, 'shadowRoot', { get() { return this } })
 
 OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\WorkflowKitinerary\\Operation',
